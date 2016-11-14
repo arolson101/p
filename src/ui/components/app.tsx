3a8b78428309b2b71bc31bbf283826f1f1765138
arolson101 <andrew@olsononline.org>
2016-11-13 22:21:54 -0600

    semantic-ui

--- src/ui/components/app.tsx
+++ src/ui/components/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { AppState, AppDispatch, LoadAllDbs } from '../../modules'
+import { Message, Icon, Button, List, Image } from 'semantic-ui-react'
 
 interface Props {
   all: string[]
@@ -11,7 +12,29 @@ interface Props {
 export const AppComponent = (props: Props) => {
   return (
     <div>app {props.all.join(', ')}
-      <button onClick={() => props.LoadAllDbs()}>load all dbs</button>
+
+      <List selection divided verticalAlign='middle'>
+        <List.Item>
+          <Image avatar src='http://semantic-ui.com/images/avatar/small/helen.jpg' />
+            <List.Content>
+              <List.Header>Helen</List.Header>
+            </List.Content>
+        </List.Item>
+        <List.Item>
+          <Image avatar src='http://semantic-ui.com/images/avatar/small/christian.jpg' />
+            <List.Content>
+              <List.Header>Christian</List.Header>
+            </List.Content>
+        </List.Item>
+        <List.Item>
+          <Image avatar src='http://semantic-ui.com/images/avatar/small/daniel.jpg' />
+            <List.Content>
+              <List.Header>Daniel</List.Header>
+            </List.Content>
+        </List.Item>
+      </List>
+
+      <Button onClick={() => props.LoadAllDbs()}>load all dbs</Button>
     </div>
   )
 }
