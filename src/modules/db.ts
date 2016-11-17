8327a3a417215e5eb7b285baca5ba8e0b42d8aed
arolson101 <andrew@olsononline.org>
2016-11-17 13:32:57 -0600

    db change stream

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -9,11 +9,15 @@ PouchDB.plugin(CryptoPouch)
 
 export interface DbState {
   current: PouchDB.Database<{}> | undefined
+  changes: PouchDB.ChangeEmitter | undefined
+  seq: number
   all: string[]
 }
 
 const initialState: DbState = {
   current: undefined,
+  changes: undefined,
+  seq: 0,
   all: []
 }
 
@@ -45,19 +49,54 @@ const SET_CURRENT_DB = 'db/setCurrent' as SET_CURRENT_DB
 
 interface SetCurrentDbAction {
   type: SET_CURRENT_DB
-  current: PouchDB.Database<{}>
+  current: PouchDB.Database<{}> | undefined
+  changes: PouchDB.ChangeEmitter | undefined
 }
 
-export const LoadDb = (name: string, password: string): SetCurrentDbAction => {
-  const current = new PouchDB(name)
-  current.crypto(password)
-  return {
-    type: SET_CURRENT_DB,
-    current
+const SetCurrentDb = (current?: PouchDB.Database<any>, changes?: PouchDB.ChangeEmitter): SetCurrentDbAction => ({
+  type: SET_CURRENT_DB,
+  current,
+  changes
+})
+
+type DB_CHANGE = 'db/changeEvent'
+const DB_CHANGE = 'db/changeEvent' as DB_CHANGE
+
+interface ChangeEventAction {
+  type: DB_CHANGE
+  db: PouchDB.Database<any>
+  seq: number
+}
+
+const ChangeEvent = (db: PouchDB.Database<any>, seq: number): ChangeEventAction => ({
+  type: DB_CHANGE,
+  db,
+  seq
+})
+
+export const LoadDb = (name: string, password: string): Thunk => {
+  return async (dispatch: Dispatch<State>) => {
+    const db = new PouchDB(name)
+    db.crypto(password)
+    const changes = db.changes({
+      since: 'now',
+      live: true
+    })
+    .on('change', (change) => {
+      dispatch(ChangeEvent(db, change.seq))
+    })
+
+    dispatch(SetCurrentDb(db, changes))
   }
 }
 
-type Actions = SetAllDbsAction | SetCurrentDbAction | { type: '' }
+export const UnloadDb = (): SetCurrentDbAction => SetCurrentDb(undefined, undefined)
+
+type Actions
+  = SetAllDbsAction
+  | SetCurrentDbAction
+  | ChangeEventAction
+  | { type: '' }
 
 const reducer = (state: DbState = initialState, action: Actions): DbState => {
   switch (action.type) {
@@ -65,7 +104,16 @@ const reducer = (state: DbState = initialState, action: Actions): DbState => {
       return Object.assign({}, state, { all: action.all } as DbState)
 
     case SET_CURRENT_DB:
-      return Object.assign({}, state, { current: action.current } as DbState)
+      if (state.changes && state.changes !== action.changes) {
+        state.changes.cancel()
+      }
+      return Object.assign({}, state, { current: action.current, changes: action.changes } as DbState)
+
+    case DB_CHANGE:
+      if (state.current !== action.db) {
+        return state
+      }
+      return Object.assign({}, state, { seq: action.seq })
 
     default:
       return state
