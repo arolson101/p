a3c2d5c46fbd7257db4195caca62d2e9dee08c47
arolson101 <andrew@olsononline.org>
2016-12-15 17:03:27 -0600

    no accounts display

--- src/ui/components/inRead.tsx
+++ src/ui/components/inRead.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import { Grid, PageHeader } from 'react-bootstrap'
+import { FormattedMessage, defineMessages } from 'react-intl'
+import { Grid, PageHeader, ButtonGroup, Button } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { DbInfo, Institution, Account } from '../../docs'
@@ -7,6 +8,13 @@ import { AppState } from '../../state'
 import { Breadcrumbs } from './breadcrumbs'
 import { RouteProps } from './props'
 import { selectDbInfo, selectInstitution } from './selectors'
+
+export const messages = defineMessages({
+  noAccounts: {
+    id: 'inRead.noAccounts',
+    defaultMessage: 'No Accounts'
+  }
+})
 
 interface Props {}
 
@@ -19,7 +27,7 @@ interface ConnectedProps {
 type AllProps = Props & RouteProps<Institution.Params> & ConnectedProps
 
 export const InReadComponent = (props: AllProps) => {
-  const { institution } = props
+  const { institution, router } = props
   const accounts = props.accounts!
   return (
     <div>
@@ -27,20 +35,26 @@ export const InReadComponent = (props: AllProps) => {
         <Grid>
           <Breadcrumbs {...props}/>
           <PageHeader>{institution.name}</PageHeader>
-          <ul>
-            {institution.accounts.map(id => accounts.get(id)).map(account => account &&
-              <li key={account._id}>
-                <Link to={Account.to.read(account)}>
-                  <i className={Account.icons[account.type]}/>
-                  {' '}
-                  {account.name}
-                </Link>
-              </li>
-            )}
-          </ul>
-          <p><Link to={Institution.to.accountCreate(institution)}>add account</Link></p>
-          <p><Link to={Institution.to.update(institution)}>update</Link></p>
-          <p><Link to={Institution.to.del(institution)}>delete</Link></p>
+          <ButtonGroup className='pull-right'>
+            <Button bsSize='small' href={router.createHref(Institution.to.accountCreate(institution))}>add account</Button>
+            <Button bsSize='small' href={router.createHref(Institution.to.update(institution))}>update</Button>
+            <Button bsSize='small' href={router.createHref(Institution.to.del(institution))}>delete</Button>
+          </ButtonGroup>
+          {institution.accounts.length > 0 ? (
+            <ul>
+              {institution.accounts.map(id => accounts.get(id)).map(account => account &&
+                <li key={account._id}>
+                  <Link to={Account.to.read(account)}>
+                    <i className={Account.icons[account.type]}/>
+                    {' '}
+                    {account.name}
+                  </Link>
+                </li>
+              )}
+            </ul>
+          ) : (
+            <FormattedMessage {...messages.noAccounts}/>
+          )}
         </Grid>
       }
     </div>
