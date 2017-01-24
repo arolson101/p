be0c66f1c947bdd5cb6e3b3c23aa88d184a49059
arolson101 <andrew@olsononline.org>
2017-01-24 16:58:01 -0600

    budget planning

---
+++ src/state/Budget.ts
@@ -0,0 +1,34 @@
+/**
+ * time period: week/month/quarter/paycheck
+ * upcoming bills in future time periods
+ * extra money in budget goes into next month's budget
+ * distribution rules?  auto-transfer for each bill, leave the rest for user
+ * how to handle debt?
+ * how to budget credit card payment?
+ * everything is an envelope?  paycheck goes into income, transfer to rest
+ *
+ *
+ * Budget (January 2017)
+ *  |- Unallocated (keep at 0)
+ *  |- + Bills
+ *  |  |- Mortgage (due 1/12) $1000
+ *  |  |- Netflix (due 1/15) $10
+ *  |  |- Vehicle registration (due 4/16) will be $100, need to add ($100-current)/time this month
+ *  |  \- Taxes (due 4/1) will be $6000, saved $5000, add $500
+ *  |- + Shared Expenses
+ *  |  |- Groceries $600
+ *  |  \- College savings $200
+ *  \- + Savings goals
+ *     |- Disney Trip $100 / $2000
+ *
+ * My Budget (January 2017)
+ *  |- Unallocated (keep at 0)
+ *  |- + Expenses
+ *  |  |- Groceries $100
+ *  |  |- Gas $100
+ *  \- + Savings Goals
+ *     |- New car - $100 / $30000
+ *     \- Playstation VR - 0 / $500
+ */
+
+export {}
