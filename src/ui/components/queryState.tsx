5e7e15b8fab853a2d7d64881a66e9b18335f14ff
arolson101 <andrew@olsononline.org>
2017-01-24 23:22:15 -0600

    better withQuerySyncedState with proper debounce

--- src/ui/components/queryState.tsx
+++ src/ui/components/queryState.tsx
@@ -1,38 +1,34 @@
-import { withRouter } from 'react-router'
-import { compose, withHandlers, /*mapPropsStream,*/ withState } from 'recompose'
-// import * as Rx from 'rxjs'
+import * as React from 'react'
 import { RouteProps } from './props'
+import debounce = require('lodash.debounce')
 
-export const withQuerySyncedState = <T extends {}>(name: string, setter: string, dflt: T, convert: (val: string) => T) => {
-  return compose(
-    withRouter,
-    withState(name, '_' + setter, (props: RouteProps<any>) => {
-      const query = props.location.query as any
-      if (name in query) {
-        return convert(query[name])
-      } else {
-        return dflt
-      }
-    }),
-    withHandlers({
-      [setter]: (props: RouteProps<any>) => (value: T) => {
-        const { ['_' + setter]: stateSetter, [name]: existingValue, location, router } = props
-        if (value !== existingValue) {
-          const nextLocation = { ...location, query: { ...location.query, [name]: value }}
-          router.replace(nextLocation)
-          stateSetter(value)
+export const withQuerySyncedState = <T extends {}>(name: string, setter: string, dflt: T, convert: (val: string) => T) =>
+  (Component: any) =>
+    class extends React.PureComponent<RouteProps<any>, any> {
+      private setValue: ((value: T) => void) & _.Cancelable
+      constructor(props?: any) {
+        super(props)
+        const query = this.props.location.query as any
+        const value = (name in query) ? convert(query[name]) : dflt
+        this.state = {
+          [name]: value
         }
+        this.setValue = debounce(
+          (value: T) => {
+            this.setState({[name]: value}, () => {
+              const { location, router } = this.props
+              const nextLocation = { ...location, query: { ...location.query, ...this.state }}
+              router.replace(nextLocation)
+            })
+          },
+          200,
+          { leading: true }
+        )
       }
-    })
-    // mapPropsStream((props$: Rx.Observable<any>) => {
-    //   const update$ = props$
-    //     // .debounceTime(500)
-    //     .distinctUntilKeyChanged(name)
-    //     .do(({ router, location, [name]: value }) => {
-    //       const nextLocation = { ...location, query: { ...location.query, [name]: value }}
-    //       router.replace(nextLocation)
-    //     })
-    //   return props$.combineLatest(update$, props => props)
-    // })
-  )
-}
+      componentWillUnmount() {
+        this.setValue.cancel()
+      }
+      render() {
+        return <Component {...this.props} {...this.state} {...{[setter]: this.setValue}} />
+      }
+    }
