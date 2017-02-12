c61bd4cefd39e78a0bdca46d321a91e1f35f0dd9
arolson101 <andrew@olsononline.org>
2017-02-11 21:46:01 -0600

    don't wrap nav account lines

--- src/ui/macOS/AppNav.tsx
+++ src/ui/macOS/AppNav.tsx
@@ -24,16 +24,16 @@ export const AppNav = ({groups, selectedId, onClick}: NavProps) =>
           background={selectedId === item.id ? 'rgba(162,162,162,0.5)' : null}
           onClick={() => onClick(item)}
         >
-          <Text color='#414141' style={{width: '100%'}}>
+          <Text color='#414141' style={{width: '100%', whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden'}}>
             <i className={item.icon + ' fa-lg'}/>
             {' '}
             {item.title}
-            {item.account &&
-              <em><small className='pull-right'>
-                <FormattedNumber value={item.account.balance} style='currency' currency='USD'/>
-              </small></em>
-            }
           </Text>
+          {item.account &&
+            <em><small className='pull-right' style={{whiteSpace: 'nowrap'}}>
+              <FormattedNumber value={item.account.balance} style='currency' currency='USD'/>
+            </small></em>
+          }
         </ListViewRow>
       )}
       </ListViewSection>
