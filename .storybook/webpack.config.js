69e59ed34e5353843e20146fe45a02a7ccd4434c
arolson101 <andrew@olsononline.org>
2017-06-30 14:57:55 -0500

    electron storybook attempt.  it didn't work

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,8 +1,11 @@
 var genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 const path = require('path');
+const fs = require("fs");
 
 module.exports = function (config, env) {
     var config = genDefaultConfig(config, env);
+
+    config.target = 'electron-renderer';
 
     config.module.rules.push({
         test: /\.tsx?$/,
@@ -27,6 +30,22 @@ module.exports = function (config, env) {
     config.resolve.extensions.push(".tsx");
     config.resolve.extensions.push(".ts");
     config.resolve.extensions.push(".js");
+
+    config.externals = {
+        'ws': true,
+        'sqlite3': 'commonjs cross-sqlcipher',
+        'pouchdb': 'commonjs pouchdb',
+        'leveldown': 'commonjs leveldown',
+        'leveldown/package': 'commonjs leveldown/package',
+    };
+
+    fs.readdirSync(path.join(__dirname, '..', 'node_modules'))
+        .filter(function(x) {
+            return ['.bin'].indexOf(x) === -1;
+        })
+        .forEach(function(mod) {
+            config.externals[mod] = 'commonjs ' + mod;
+    });
 
     return config;
 };
