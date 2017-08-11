4a4348e00014a2c264f22c9786eb72ab6d6d623f
arolson101 <andrew@olsononline.org>
2017-08-11 12:48:28 -0500

    first working storybook story

--- stories/index.tsx
+++ stories/index.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
-import { BankDialog } from '../src/ui/dialogs'
+import { DatePicker } from '../src/ui/components/DatePicker'
+import { IntlProvider } from 'react-intl'
+import { Provider } from 'react-redux'
+import { createAppStore, AppInit, ImportsState, syncProviders } from 'core'
+import createHistory from 'history/createHashHistory'
+import * as moment from 'moment'
 
 const action = (str: string) => () => console.log(str)
 
+const history = createHistory()
+const store = createAppStore(history)
+let value = moment().format('L')
+
 storiesOf('Button', module)
   .add('AccountDialog', () => (
-    <BankDialog show={true} onHide={() => null} />
+    <Provider store={store}>
+      <IntlProvider locale={'en'}>
+        <DatePicker value={value} onChange={(x) => value = x} />
+      </IntlProvider>
+    </Provider>
   ))
   .add('with some emoji', () => (
     <button onClick={action('clicked')}>😀 😎 👍 💯</button>
