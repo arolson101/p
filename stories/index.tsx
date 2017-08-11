7c6fdb3ef4536a6b044201dc0c58422209ce99e2
arolson101 <andrew@olsononline.org>
2017-08-11 14:36:56 -0500

    notes & knobs & actions

--- stories/index.tsx
+++ stories/index.tsx
@@ -1,26 +1,38 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { WithNotes } from '@storybook/addon-notes'
 import { DatePicker } from '../src/ui/components/DatePicker'
 import { IntlProvider } from 'react-intl'
 import { Provider } from 'react-redux'
 import { createAppStore, AppInit, ImportsState, syncProviders } from 'core'
 import createHistory from 'history/createHashHistory'
 import * as moment from 'moment'
-
-const action = (str: string) => () => console.log(str)
+import { withKnobs, select } from '@storybook/addon-knobs'
 
 const history = createHistory()
 const store = createAppStore(history)
 let value = moment().format('L')
 
-storiesOf('Button', module)
-  .add('AccountDialog', () => (
-    <Provider store={store}>
-      <IntlProvider locale={'en'}>
-        <DatePicker value={value} onChange={(x) => value = x} />
-      </IntlProvider>
-    </Provider>
-  ))
-  .add('with some emoji', () => (
-    <button onClick={action('clicked')}>😀 😎 👍 💯</button>
-  ))
+const stories = storiesOf('Knobs', module)
+stories.addDecorator(withKnobs)
+
+stories.add('AccountDialog', () => {
+  const locale = select('locale', ['en', 'fr'], 'en')
+  return (
+    <WithNotes notes='this is a datepicker'>
+      <Provider store={store}>
+        <IntlProvider locale={locale}>
+          <div>
+            <div>{locale}</div>
+            <DatePicker value={value} onChange={(x) => value = x} />
+          </div>
+        </IntlProvider>
+      </Provider>
+    </WithNotes>
+  )
+})
+
+stories.add('with some emoji', () => (
+  <button onClick={action('clicked')}>😀 😎 👍 💯</button>
+))
