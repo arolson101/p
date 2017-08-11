c95ecbedfc9ac63d516ef6355f94a201c59dfeb2
arolson101 <andrew@olsononline.org>
2017-08-11 09:58:39 -0500

    storybook almost working

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require("webpack");
 var genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 const path = require('path');
 const fs = require("fs");
@@ -5,47 +6,61 @@ const fs = require("fs");
 module.exports = function (config, env) {
     var config = genDefaultConfig(config, env);
 
-    config.target = 'electron-renderer';
+    config.target = 'web';
 
     config.module.rules.push({
         test: /\.tsx?$/,
         exclude: /node_modules/,
         include: [/stories/, /components/, /src/],
-        use: [
-            'babel-loader',
-            'awesome-typescript-loader'
+        loaders: [
+            'react-hot-loader/webpack',
+            'ts-loader?configFileName=tsconfig.app.json'
         ]
     })
 
-    config.module.rules.push({
-        test:/\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        include: [/stories/, /components/],
-        query: {
-            presets: ['react', 'es2015', "stage-0"]
-        }
-    })
+    // config.module.rules.push({
+    //     test:/\.js$/,
+    //     loader: 'babel-loader',
+    //     exclude: /node_modules/,
+    //     include: [/stories/, /components/],
+    //     query: {
+    //         presets: ['react', 'es2015', "stage-0"]
+    //     }
+    // })
 
     config.resolve.extensions.push(".tsx");
     config.resolve.extensions.push(".ts");
     config.resolve.extensions.push(".js");
 
-    config.externals = {
-        'ws': true,
-        'sqlite3': 'commonjs cross-sqlcipher',
-        'pouchdb': 'commonjs pouchdb',
-        'leveldown': 'commonjs leveldown',
-        'leveldown/package': 'commonjs leveldown/package',
-    };
+    config.resolve.modules = [
+        path.resolve(__dirname, "..", "src"),
+        "node_modules",
+    ]
 
-    fs.readdirSync(path.join(__dirname, '..', 'node_modules'))
-        .filter(function(x) {
-            return ['.bin'].indexOf(x) === -1;
-        })
-        .forEach(function(mod) {
-            config.externals[mod] = 'commonjs ' + mod;
-    });
+    config.plugins.push(
+        // new webpack.DllReferencePlugin({
+        //     context: path.resolve(__dirname, ".."),
+        //     name: 'electron_dll',
+        //     manifest: require("../app/dev_electron_dll.json"),
+        //     sourceType: "commonsjs2"
+        //   }),
+    )
+
+    // config.externals = {
+    //     'ws': true,
+    //     'sqlite3': 'commonjs cross-sqlcipher',
+    //     'pouchdb': 'commonjs pouchdb',
+    //     'leveldown': 'commonjs leveldown',
+    //     'leveldown/package': 'commonjs leveldown/package',
+    // };
+
+    // fs.readdirSync(path.join(__dirname, '..', 'node_modules'))
+    //     .filter(function(x) {
+    //         return ['.bin'].indexOf(x) === -1;
+    //     })
+    //     .forEach(function(mod) {
+    //         config.externals[mod] = 'commonjs ' + mod;
+    // });
 
     return config;
 };
